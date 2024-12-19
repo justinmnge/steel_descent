31f2e9a9cb17f1c33440d8446e5720ccd8671c8c
Justin Monge <justin.mnge@gmail.com>
2024-12-19 14:58:07 -0400

    feat ✨: Added audio, images and data files for the game.

---
+++ data/tilesets/water.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="water" tilewidth="64" tileheight="64" tilecount="28" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../graphics/tilesets/tile_0077_water4.png" width="64" height="64"/>
+ </tile>
+ <tile id="1">
+  <image source="../graphics/tilesets/tile_0078_water5.png" width="64" height="64"/>
+ </tile>
+ <tile id="2">
+  <image source="../graphics/tilesets/tile_0079_water6.png" width="64" height="64"/>
+ </tile>
+ <tile id="3">
+  <image source="../graphics/tilesets/tile_0080_water7.png" width="64" height="64"/>
+ </tile>
+ <tile id="4">
+  <image source="../graphics/tilesets/tile_0081_water8.png" width="64" height="64"/>
+ </tile>
+ <tile id="5">
+  <image source="../graphics/tilesets/tile_0082_water9.png" width="64" height="64"/>
+ </tile>
+ <tile id="6">
+  <image source="../graphics/tilesets/tile_0083_water10.png" width="64" height="64"/>
+ </tile>
+ <tile id="7">
+  <image source="../graphics/tilesets/tile_0084_water11.png" width="64" height="64"/>
+ </tile>
+ <tile id="8">
+  <image source="../graphics/tilesets/tile_0085_water12.png" width="64" height="64"/>
+ </tile>
+ <tile id="9">
+  <image source="../graphics/tilesets/tile_0086_water13.png" width="64" height="64"/>
+ </tile>
+ <tile id="10">
+  <image source="../graphics/tilesets/tile_0087_water14.png" width="64" height="64"/>
+ </tile>
+ <tile id="11">
+  <image source="../graphics/tilesets/tile_0088_water15.png" width="64" height="64"/>
+ </tile>
+ <tile id="12">
+  <image source="../graphics/tilesets/tile_0089_water16.png" width="64" height="64"/>
+ </tile>
+ <tile id="13">
+  <image source="../graphics/tilesets/tile_0090_water17.png" width="64" height="64"/>
+ </tile>
+ <tile id="14">
+  <image source="../graphics/tilesets/tile_0091_water18.png" width="64" height="64"/>
+ </tile>
+ <tile id="15">
+  <image source="../graphics/tilesets/tile_0092_water19.png" width="64" height="64"/>
+ </tile>
+ <tile id="16">
+  <image source="../graphics/tilesets/tile_0093_water20.png" width="64" height="64"/>
+ </tile>
+ <tile id="17">
+  <image source="../graphics/tilesets/tile_0094_water21.png" width="64" height="64"/>
+ </tile>
+ <tile id="18">
+  <image source="../graphics/tilesets/tile_0095_water22.png" width="64" height="64"/>
+ </tile>
+ <tile id="19">
+  <image source="../graphics/tilesets/tile_0096_water23.png" width="64" height="64"/>
+ </tile>
+ <tile id="20">
+  <image source="../graphics/tilesets/tile_0097_water24.png" width="64" height="64"/>
+ </tile>
+ <tile id="21">
+  <image source="../graphics/tilesets/tile_0098_water25.png" width="64" height="64"/>
+ </tile>
+ <tile id="22">
+  <image source="../graphics/tilesets/tile_0099_water26.png" width="64" height="64"/>
+ </tile>
+ <tile id="23">
+  <image source="../graphics/tilesets/tile_0100_water27.png" width="64" height="64"/>
+ </tile>
+ <tile id="24">
+  <image source="../graphics/tilesets/tile_0101_water28.png" width="64" height="64"/>
+ </tile>
+ <tile id="25">
+  <image source="../graphics/tilesets/tile_0074_water1.png" width="64" height="64"/>
+ </tile>
+ <tile id="26">
+  <image source="../graphics/tilesets/tile_0075_water2.png" width="64" height="64"/>
+ </tile>
+ <tile id="27">
+  <image source="../graphics/tilesets/tile_0076_water3.png" width="64" height="64"/>
+ </tile>
+</tileset>
